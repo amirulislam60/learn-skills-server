2dc4828b9b8c3ae5ac12b52b29c2446f83877872
amirulislam <amirulislam5181@gmail.com>
2022-10-27 09:17:59 +0600

    img add

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const category = require('./data/catagory.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('Online courses API')
@@ -13,6 +14,23 @@ app.get('/', (req, res) => {
 
 app.get('/courses-category', (req, res) => {
     res.send(category)
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/category/:id',(req, res)=> {
+    const id = req.params.id;
+    const category_courses = courses.filter(c => c.category_id===id);
+    res.send(category_courses);
+})
+
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const SelectedCourses = courses.find(c => c.id === id);
+    res.send(SelectedCourses);
+    console.log(req.params.id)
 })
 
 app.listen(port, () => {
